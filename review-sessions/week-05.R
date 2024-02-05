dc8a61e32bca6de866e235fff4efb10e7bc2da68
Jesse Lecy <jdlecy@gmail.com>
2024-02-05 14:47:24 -0700

    Update week-05.R

--- review-sessions/week-05.R
+++ review-sessions/week-05.R
@@ -115,6 +115,130 @@ CSS
 </html>
 
 
+~~~~~~~~~ LIQUID TAGS 
+
+# R VERSION OF LOOP TO CREATE HTML TAGS 
+
+person <- rep( c("Ryan Reynolds","Ryan Gosling"), each=3 )
+strengths <- c("good father","funny","dated alanis morissette","builds houses","is real boy","never dated morissette")
+             
+for( i in unique(person) )
+{
+  cat( paste0( "<h3>", i, "</h3> \n\n") )
+  strengths.i <- strengths[ person == i ]
+  for( j in strengths.i )
+  {
+    cat( paste0( "<li>", j, "</li> \n" ) )
+  }
+  cat("\n\n")
+}                                                
+
+
+<h3>Ryan Reynolds</h3> 
+
+<li>good father</li> 
+<li>funny</li> 
+<li>dated alanis morissette</li> 
+
+<h3>Ryan Gosling</h3> 
+
+<li>builds houses</li> 
+<li>is real boy</li> 
+<li>never dated morissette</li>
+
+
+# JEKYLL VERSION OF LOOP TO CREATE HTML TAGS
+
+---
+layout: liquid-table
+
+reynolds:
+  strengths:
+  - good father
+  - funny
+  - dated alanis morissette
+  weaknesses: 
+  - singing
+  - green lantern movie
+  - tennis backhand 
+                                                      
+gosling:
+  strengths: 
+  - builds houses
+  - is a real boy
+  - never dated alanis morissette
+  weaknesses: 
+  - micky mouse club
+  - cries a lot
+  - not ryan reynolds
+---
+
+
+<table id="ryan-v-ryan">
+
+<thead>
+  <tr>
+    <th>  <h3>  Ryan Reynolds  </h3>  </th>
+    <th>  <h3>  Ryan Gosling  </h3>  </th>
+  </tr>
+</thead>
+
+<tbody>
+<tr>
+  <td>
+    <h4>  Strengths  </h4>
+    <ul>
+
+      ###  LIQUID LOOPS WITH YAML DATA
+      ###  reynolds:
+      ###    strengths:
+      ###    - good father
+      ###    - funny
+      ###    - dated alanis morissette
+
+      {% for item in page.reynolds.strengths %}
+         <li>  {{ item }}  </li>
+      {% endfor %}
+
+      ###    LIQUID LOOP CREATES HTML CODES:
+      ###    <li> good father </li>
+      ###    <li> funny </li>
+      ###    <li> dated alanis morissette </li>        
+
+    </ul>
+    <br>
+    <h4>  Weaknessess  </h4>
+    <ul>
+
+      {% for item in page.reynolds.weaknesses %}
+         <li>  {{ item }}  </li>
+      {% endfor %}
+
+    </ul>  
+  </td>
+  <td>
+    <h4>  Strengths  </h4>
+    <ul>
+
+      {% for item in page.gosling.strengths %}
+        <li>  {{ item }}  </li>
+      {% endfor %}
+
+    </ul>
+    <br>
+    <h4>  Weaknessess  </h4>
+    <ul>
+
+      {% for item in page.gosling.weaknesses %}
+         <li>  {{ item }}  </li>
+      {% endfor %}
+
+    </ul>
+  </td>
+</tr> 
+</table>
+
+
 
 
 
